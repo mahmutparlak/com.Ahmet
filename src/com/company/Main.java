aff4561c68fa738bd869f8061a6a406f2dada0c1
mahmutparlak <mahmutparlak@gmail.com>
2022-01-16 02:38:50 -0600

    JIRA-126 Last changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,31 @@
 package com.company;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Kaan");
-        System.out.println("Kivircik");
-        System.out.println("problem solved");
-        System.out.println("Kivrik");
-        for(int i = 0; i < 5; i++) {
-            System.out.println("gel hele");
+        System.out.println(removeDuplicate("AAHSHSFFVDHY"));
+    }
+
+    public static StringBuffer removeDuplicate(String duplicateString) {
+
+        char[] chars = duplicateString.toCharArray();
+
+        Set<Character> charSet = new LinkedHashSet<>();
+
+        for (char c : chars) {
+            charSet.add(c);
         }
+
+        System.out.println(charSet);
+
+        StringBuffer clearString = new StringBuffer();
+
+        for (Character character : charSet) {
+            clearString.append(character);
+        }
+        return clearString;
     }
 }
